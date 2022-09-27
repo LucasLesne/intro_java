abfb9cbf8837d3122cc3002792231842928c6115
LucasLesne <lesnelulu@gmail.com>
2022-09-27 11:41:42 +0200

    création programme

--- src/main/java/fr/dampierre/Exercice1.java
+++ src/main/java/fr/dampierre/Exercice1.java
@@ -6,7 +6,14 @@ public class Exercice1 {
     
     public static void main(String[] args) {
     
-        
+        int heure = 12;
+        int minutes = 34;
+        int secondes = 56;
+        int tempsEcoule = heure*60*60 + minutes*60 + secondes;
+        System.out.println("Il est " + heure + " h " + minutes + " min " + secondes + "s.");
+        System.out.println("Il s'est écoulé " + tempsEcoule + " secondes depuis minuit.");
+
     
     
     }
+}
