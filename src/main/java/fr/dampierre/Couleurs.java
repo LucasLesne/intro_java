0d5660e897fd69dc869eae1b90d1a2ada38f4242
LucasLesne <lesnelulu@gmail.com>
2022-10-05 13:42:02 +0200

    first commit

---
+++ src/main/java/fr/dampierre/Couleurs.java
@@ -0,0 +1,24 @@
+package fr.dampierre;
+
+import java.util.Scanner;
+
+public class Couleurs {
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("Entrez votre couleur (r,v,B)");
+        String couleur = clavier.nextLine();
+        clavier.close();
+            if(couleur.equals("r")||couleur.equals("R")){
+                System.out.println("Vous avez choisi le rouge.");
+            }
+            else if(couleur.equals("v")||couleur.equals("V")){
+                System.out.println("Vous avez choisi le vert.");
+            }
+            else if(couleur.equals("b")||couleur.equals("B")){
+                System.out.println("Vous avez choisi le bleu.");
+            }
+            else
+                System.out.println("Connais pas.");
+
+    }
+}
