969af4efae6f7f1774dc7eb9f686f2350c462684
LucasLesne <lesnelulu@gmail.com>
2022-10-06 10:06:18 +0200

    new

---
+++ src/main/java/fr/dampierre/Jeu_de_loie.java
@@ -0,0 +1,16 @@
+package fr.dampierre;
+
+import java.util.Random;
+
+public class Jeu_de_loie {
+   
+    public static void main(String[] args) {
+    
+        Random generateur = new Random();
+    for(int i=0; i<5; i++){
+
+    }
+    
+    }
+
+}
