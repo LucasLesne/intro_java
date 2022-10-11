fd8f4082ba89d5ec3869f4f98d0d38f0eaea7533
LucasLesne <lesnelulu@gmail.com>
2022-10-11 11:12:55 +0200

    fini

--- src/main/java/fr/dampierre/Jeu_de_loie.java
+++ src/main/java/fr/dampierre/Jeu_de_loie.java
@@ -1,25 +1,24 @@
 package fr.dampierre;
-
 import java.util.Random;
-
-public class Jeu_de_loie {
-   
+public class Jeu_de_loie {  
     public static void main(String[] args) {
     
         Random generateur = new Random();
         int points=0;
-
-    for(int i=0; i<5; i++){
+    
+    while(points!=20){
+        points=0;
+        for(int i=0; i<5; i++){
         int lancer = generateur.nextInt(6) + 1;
+        System.out.println("Vous avez fait "+lancer);
         points = points + lancer;
         System.out.println("Vous êtes à la case " + points);
-    }
-    if(points==20){
-        System.out.println("Gagné");
-    }
-    else{
+        }
+        if(points!=20){
         System.out.println("Perdu");
+        }
     }
+     System.out.println("Gagné");
+    
     }
-
 }
