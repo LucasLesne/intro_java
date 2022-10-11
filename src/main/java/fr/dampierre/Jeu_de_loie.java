f5c0c3784edb437b48f08e24d3792743d3381444
LucasLesne <lesnelulu@gmail.com>
2022-10-11 10:53:03 +0200

    creation

--- src/main/java/fr/dampierre/Jeu_de_loie.java
+++ src/main/java/fr/dampierre/Jeu_de_loie.java
@@ -7,10 +7,19 @@ public class Jeu_de_loie {
     public static void main(String[] args) {
     
         Random generateur = new Random();
-    for(int i=0; i<5; i++){
+        int points=0;
 
+    for(int i=0; i<5; i++){
+        int lancer = generateur.nextInt(6) + 1;
+        points = points + lancer;
+        System.out.println("Vous êtes à la case " + points);
     }
-    
+    if(points==20){
+        System.out.println("Gagné");
+    }
+    else{
+        System.out.println("Perdu");
+    }
     }
 
 }
