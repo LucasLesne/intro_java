6c4f2d329d0a36dc3180cfc93227711381117a5c
LucasLesne <lesnelulu@gmail.com>
2022-10-20 09:30:57 +0200

    attentat

---
+++ src/main/java/fr/dampierre/Motif4.java
@@ -0,0 +1,16 @@
+package fr.dampierre;
+
+public class Motif4 {
+    public static void main(String[] args) {
+    int i = 1;
+            while(i<7) {
+                for (int j = 0; j < i; j++) {
+                     
+                System.out.print(i);
+                }
+             System.out.println();
+             i = i+1;
+            }
+           
+    }
+}
