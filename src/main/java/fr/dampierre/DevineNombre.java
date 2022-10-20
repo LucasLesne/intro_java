bba41130b2e3162f555765c12b98b32d5eada919
LucasLesne <lesnelulu@gmail.com>
2022-10-20 12:51:00 +0200

    V7

--- src/main/java/fr/dampierre/DevineNombre.java
+++ src/main/java/fr/dampierre/DevineNombre.java
@@ -7,36 +7,34 @@ Random generateur = new Random();
 Scanner clavier = new Scanner(System.in);
 int nombre; int tentative; int reponse; 
 String rejouer = "o";
-
-while(rejouer == "o"){
-tentative = 0; reponse = 0;
-nombre = generateur.nextInt(100)+1;
-System.out.println("Je pense à un nombre entre 1 et 100 inclus. Devinez lequel.");
-    while(reponse!=nombre){
-        System.out.print("Entrez un nombre : ");
-        reponse = clavier.nextInt();
+while(rejouer.equals("o")){
+    tentative = 0; reponse = 0;
+    nombre = generateur.nextInt(100)+1;
+    System.out.println("Je pense à un nombre entre 1 et 100 inclus. Devinez lequel.");
+        while(reponse!=nombre){
+            System.out.print("Entrez un nombre : ");
+            reponse = clavier.nextInt();
         
-        if(reponse<1 || reponse>100){
-            System.out.println("Merci d'entrer un nombre entre 1 et 100.");
-        }
-        else if
-        (reponse<nombre){
-            System.out.println("C'est plus.");
-            tentative=tentative+1;
+            if(reponse<1 || reponse>100){
+                System.out.println("Merci d'entrer un nombre entre 1 et 100.");
             }
-        else if(reponse>nombre){
-            System.out.println("C'est moins");
-            tentative=tentative+1;
+            else if(reponse<nombre){
+                System.out.println("C'est plus.");
+                tentative=tentative+1;
+            }
+            else if(reponse>nombre){
+                System.out.println("C'est moins");
+                tentative=tentative+1;
             }
         }
     System.out.println("Quel bol, vous avez trouvé !");
     System.out.println("Il vous aura fallu "+tentative + " tentatives.");
     System.out.print("On continue ? O/N ");
-    rejouer =clavier.next().toLowerCase().substring(0,1);
-    System.out.println(rejouer);
-        
+    rejouer = clavier.next().toLowerCase().substring(0,1);
     
+   
 }
+System.out.println("Merci d'avoir joué !");
 clavier.close();
         
 }
