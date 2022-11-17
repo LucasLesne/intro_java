448991f4cb2082855878988e051e368f60d64242
LucasLesne <lesnelulu@gmail.com>
2022-11-17 12:08:57 +0100

    new

--- src/main/java/fr/dampierre/DevineNombre2.java
+++ src/main/java/fr/dampierre/DevineNombre2.java
@@ -1,35 +1,32 @@
 package fr.dampierre;
 import java.util.Random;
-import java.util.Scanner;
 public class DevineNombre2 {
     public static void main(String[] args) {
 Random generateur = new Random();
-Scanner clavier = new Scanner(System.in);
-int nombre; int tentative; int reponse; 
+int nombre; int tentative; double moyenne=0; int reponse; int nbParties=10000;
 
 
-for (int i = 0; i < 4; i++) {
+for (int i = 1; i < nbParties+1; i++) {
     tentative = 0; reponse = 0;
     nombre = generateur.nextInt(100)+1;
-    System.out.println("Je pense à un nombre et j'essaie de deviner lequel sans tricher.");
+    System.out.print(i+" - ");
         while(reponse!=nombre){
             reponse = generateur.nextInt(100)+1;
-            System.out.println("Je tente : "+reponse);
+            System.out.print(reponse+" ");
             if(reponse<nombre){
-                System.out.println("C'est plus.");
-            }
-            if(reponse>nombre){
-                System.out.println("C'est moins");
                 
             }
-            tentative=tentative+1;
+            if(reponse>nombre){
+                
+            }
+        tentative=tentative+1;
         }
-    System.out.println("Quel bol, vous avez trouvé !");
-    System.out.println("Il m'a fallu "+tentative + " tentatives.");
-   
+
+    System.out.println("["+tentative + "]");
+    moyenne=moyenne+tentative; 
 }
-System.out.println("Merci d'avoir joué !");
-clavier.close();
+System.out.println("En moyenne, sur " + nbParties + " parties, il m'a fallu "+ moyenne/nbParties + " tentatives.");
+
         
 }
 }
